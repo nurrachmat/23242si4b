05d7a65424daef9319a10a641058ca5e697af74d
Nur Rachmat <>
2024-04-16 16:58:09 +0700

    localStorage lanjutan

--- javascript/latihanLS/script.js
+++ javascript/latihanLS/script.js
@@ -22,4 +22,24 @@ btnSimpan.addEventListener("click", function () {
   }
   // simpan ke dalam localStorage
   localStorage.setItem("testimoni", JSON.stringify(data));
+  // panggil getData()
+  getData();
 });
+
+// tampilkan data localStorage
+function getData() {
+  // clear element list
+  list.innerHTML = "";
+
+  if (localStorage.getItem("testimoni")) {
+    // ada localStorage dengan key testimoni
+    data = JSON.parse(localStorage.getItem("testimoni"));
+    for (const [index, row] of data.entries()) {
+      // list.innerHTML += `<li>${row.nama} ${row.testimoni}</li>`;
+      list.innerHTML += `<tr><td>${row.nama}</td><td>${row.testimoni}</td></tr>`;
+    }
+  }
+}
+
+// panggil getData()
+getData();
